bc2e73e68346422f018068008b681f766576a014
Festus <festusyuma@gmail.com>
2024-10-31 01:02:23 +0100

    added deno api construct

---
+++ packages/app-construct/src/lib/deno-api-construct.ts
@@ -0,0 +1,116 @@
+import {  Attachable, Grantable } from '@fy-stack/types';
+import { Duration } from 'aws-cdk-lib';
+import type { HttpRouteIntegration } from 'aws-cdk-lib/aws-apigatewayv2';
+import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+import {
+  AllowedMethods,
+  BehaviorOptions,
+  CachePolicy,
+  OriginRequestPolicy,
+  ResponseHeadersPolicy,
+  ViewerProtocolPolicy,
+} from 'aws-cdk-lib/aws-cloudfront';
+import { FunctionUrlOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { Platform } from 'aws-cdk-lib/aws-ecr-assets';
+import {
+  Code,
+  Function,
+  FunctionUrlAuthType,
+  Handler,
+  Runtime,
+} from 'aws-cdk-lib/aws-lambda';
+import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
+import { ITopicSubscription, SubscriptionProps } from 'aws-cdk-lib/aws-sns';
+import {
+  LambdaSubscription,
+  SqsSubscription,
+} from 'aws-cdk-lib/aws-sns-subscriptions';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
+import { Construct } from 'constructs';
+
+import { AppConstruct, AppProperties } from './types';
+import { lambdaAttach } from './utils/lambda-attach';
+import { lambdaGrant } from './utils/lambda-grant';
+
+export class DenoApiConstruct
+  extends Construct
+  implements AppConstruct
+{
+  public function: Function;
+  public queue: Queue | undefined;
+
+  constructor(scope: Construct, id: string, props: AppProperties) {
+    super(scope, id);
+
+    this.function = new Function(this, `AppFunction`, {
+      memorySize: 512,
+      timeout: Duration.seconds(30),
+      code: Code.fromAssetImage(props.output, {
+        file: props.buildPaths.dockerFile,
+        platform: Platform.LINUX_AMD64,
+      }),
+      handler: Handler.FROM_IMAGE,
+      runtime: Runtime.FROM_IMAGE,
+    });
+
+    if (props.queue) {
+      this.queue = new Queue(this, 'AppQueue', {
+        visibilityTimeout: Duration.seconds(59),
+      });
+
+      this.function.addEventSource(
+        new SqsEventSource(this.queue, {
+          batchSize: props.queue.batchSize,
+        })
+      );
+    }
+  }
+
+  attach(attachable: Record<string, Attachable>): void {
+    return lambdaAttach(this.function, attachable);
+  }
+
+  grant(...grantables: Grantable[]): void {
+    return lambdaGrant(this.function, grantables);
+  }
+
+  subscription(props: SubscriptionProps): ITopicSubscription {
+    if (this.queue) {
+      return new SqsSubscription(this.queue, props);
+    }
+
+    return new LambdaSubscription(this.function, props);
+  }
+
+  cloudfront(path: string): Record<string, BehaviorOptions> {
+    this.function.addEnvironment('BASE_PATH', path);
+
+    const apiUrl = this.function.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+    });
+
+    const apiBehavior = {
+      origin: new FunctionUrlOrigin(apiUrl),
+      cachePolicy: CachePolicy.CACHING_DISABLED,
+      allowedMethods: AllowedMethods.ALLOW_ALL,
+      compress: true,
+      viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      originRequestPolicy: OriginRequestPolicy.ALL_VIEWER_EXCEPT_HOST_HEADER,
+      responseHeadersPolicy:
+      ResponseHeadersPolicy.CORS_ALLOW_ALL_ORIGINS_WITH_PREFLIGHT_AND_SECURITY_HEADERS,
+    };
+
+    return { [`${path}/*`]: apiBehavior };
+  }
+
+  api(path: string): Record<string, HttpRouteIntegration> {
+    this.function.addEnvironment('BASE_PATH', path);
+
+    const integration = new HttpLambdaIntegration(
+      'AppIntegration',
+      this.function
+    );
+
+    return { [`${path}/*`]: integration };
+  }
+}
