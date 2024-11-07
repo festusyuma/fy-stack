e90e8d9a8a21dd464240f1a0f775d640db9fcf5e
Festus <festusyuma@gmail.com>
2024-11-07 20:35:51 +0100

    refactor auora db to regular db instance

--- packages/database-construct/src/lib/database-construct.ts
+++ packages/database-construct/src/lib/database-construct.ts
@@ -1,48 +1,62 @@
-import { Attachable, Grantable } from '@fy-stack/types';
+import type { Attachable, Grantable } from '@fy-stack/types';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { IGrantable } from 'aws-cdk-lib/aws-iam';
 import * as rds from 'aws-cdk-lib/aws-rds';
+import { DatabaseInstanceEngine } from 'aws-cdk-lib/aws-rds';
 import * as secretsManager from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
 
+import { DatabaseConstructProps } from './types';
+
+/**
+ * Represents a database construct that provisions an RDS database instance along with associated secrets.
+ * It implements both {@link Attachable `Attachable`} and {@link Grantable `Grantable`} interfaces.
+ */
 export class DatabaseConstruct
   extends Construct
   implements Attachable, Grantable
 {
   public dbSecrets: secretsManager.ISecret;
-  public db: rds.IDatabaseCluster;
+  public db: rds.DatabaseInstance;
   public dbName: string;
 
-  constructor(scope: Construct, id: string) {
+  constructor(scope: Construct, id: string, props?: DatabaseConstructProps) {
     super(scope, id);
-
-    const db = rds.DatabaseCluster.fromDatabaseClusterAttributes(
-      this,
-      'DatabaseCluster',
-      { clusterIdentifier: 'dev-db-instance' }
-    );
 
     this.dbName = `${this.node.id}-db`;
 
-    const dbSecret = new rds.DatabaseSecret(this, 'DatabaseSecret', {
-      username: this.node.id,
-      dbname: this.dbName,
+    const vpc = ec2.Vpc.fromLookup(
+      this,
+      'VPC',
+      props?.vpcId ? { vpcId: props.vpcId } : { isDefault: true }
+    );
+
+    const credentials = rds.Credentials.fromGeneratedSecret(
+      `${this.dbName}-user`,
+      {}
+    );
+    if (!credentials.secret)
+      throw new Error('Could not create database credentials');
+
+    this.dbSecrets = credentials.secret;
+
+    this.db = new rds.DatabaseInstance(this, 'DB', {
+      vpc,
+      engine: props?.engine ?? DatabaseInstanceEngine.POSTGRES,
+      instanceType: ec2.InstanceType.of(
+        props?.instance?.class ?? ec2.InstanceClass.T3,
+        props?.instance?.size ?? ec2.InstanceSize.NANO
+      ),
+      publiclyAccessible: props?.public,
+      databaseName: this.dbName,
+      credentials,
+      ...(props?.additionalData ?? {}),
     });
-
-    dbSecret.attach(db);
-
-    this.db = db;
-    this.dbSecrets = dbSecret;
   }
 
   grantable(grant: IGrantable) {
-    grant.grantPrincipal.addToPrincipalPolicy(
-      new iam.PolicyStatement({
-        effect: iam.Effect.ALLOW,
-        actions: ['rds-data:*'],
-        resources: [this.db.clusterArn],
-      })
-    );
+    this.db.grantConnect(grant);
 
     grant.grantPrincipal.addToPrincipalPolicy(
       new iam.PolicyStatement({
@@ -55,7 +69,7 @@ export class DatabaseConstruct
 
   attachable() {
     return {
-      arn: this.db.clusterArn,
+      arn: this.db.instanceArn,
       name: this.dbName,
       secretsArn: this.dbSecrets.secretArn,
       secretsName: this.dbSecrets.secretName,
---
+++ packages/database-construct/src/lib/types.ts
@@ -0,0 +1,26 @@
+import type { InstanceClass, InstanceSize } from 'aws-cdk-lib/aws-ec2';
+import type {
+  DatabaseInstanceProps,
+  IInstanceEngine,
+} from 'aws-cdk-lib/aws-rds';
+
+/**
+ * Properties required for setting up a database construct.
+ */
+export type DatabaseConstructProps = {
+  /** Optionally pass in existing VPC id */
+  vpcId?: string
+  /**
+   *  Define specific RDS instance {@link IInstanceEngine engine}
+   */
+  engine?: IInstanceEngine;
+  /** Define specific RDS instance {@link InstanceClass class} and {@link InstanceSize size} to use. */
+  instance?: { class: InstanceClass; size: InstanceSize };
+  /** Make database public */
+  public?: boolean;
+  /** Other RDS properties {@link DatabaseInstanceProps} */
+  additionalData?: Omit<
+    DatabaseInstanceProps,
+    'instanceType' | 'databaseName' | 'publiclyAccessible' | 'engine'
+  >;
+};
