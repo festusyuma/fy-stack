307e020de315dd8ef065bbfbd92f6ff64f513fbd
Festus <festusyuma@gmail.com>
2025-01-10 21:49:57 +0100

    created task-construct for ecr task definition provision

---
+++ packages/task-construct/src/lib/types.ts
@@ -0,0 +1,10 @@
+import type { IVpc } from 'aws-cdk-lib/aws-ec2';
+import type { AssetImageProps, ContainerDefinitionOptions, FargateTaskDefinitionProps } from 'aws-cdk-lib/aws-ecs';
+
+export type TaskConstructsProps = FargateTaskDefinitionProps & {
+  vpc: IVpc
+  clusterArn: string;
+  env?: Record<string, string>;
+  output: string;
+  defaultImage?: Omit<ContainerDefinitionOptions, "image" | "logging"> & { image: AssetImageProps }
+}
